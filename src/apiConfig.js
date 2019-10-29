d621fa8548646fee1292d691f282d750b41951e5
apcaplan <apcaplan@gmail.com>
2019-10-28 22:14:36 -0400

    update url for deployed api

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://quiet-tundra-41293.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
